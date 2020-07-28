462eba7d53602467ea964974c1c0e617513495a6
ghpark <ghrpark@dtnsm.com>
2020-07-28 21:03:22 +0900

    logback(slf4j), token properties(ignore)

--- src/main/java/com/hongsong/bot/BotApplication.java
+++ src/main/java/com/hongsong/bot/BotApplication.java
@@ -2,12 +2,46 @@ package com.hongsong.bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-public class BotApplication {
+public class BotApplication extends ListenerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(BotApplication.class);
+    private final Properties properties;
 
-    public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("token").build();
+    public BotApplication() {
+        properties = new Properties();
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void loadProperties(String path) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(path);
+        properties.load(inputStream);
+        inputStream.close();
+    }
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        BotApplication botApplication = new BotApplication();
+
+        try {
+            logger.info(">>>>> Application load properties");
+            botApplication.loadProperties("/application.yml");
+        } catch (IOException e) {
+            logger.error("!!!!! Application load properties error : ", e);
+        }
+        Properties properties = botApplication.getProperties();
+
+        JDA jda = JDABuilder.createDefault(properties.getProperty("token")).build();
+        jda.awaitReady();
+        jda.getTextChannelsByName("general", true).forEach(textChannel -> textChannel.sendMessage("이 몸, 등장.").queue());
     }
 }
